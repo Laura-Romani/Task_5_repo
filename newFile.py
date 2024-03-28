db861d05ea1e56c0af045a81a370e4f280a343d1
Laura Romani <lauretta.86@hotmail.it>
2024-03-28 14:20:35 +0000

    changes

--- newFile.py
+++ newFile.py
@@ -1,4 +1,10 @@
-print("Hello world!")
-with open ("drivers.txt", "r") as file:
+#print("Hello world!")
+#with open ("drivers.txt", "r") as file:
+    #data = file.read()
+    #print(data)
+
+# program updated to reflect txt file name changes
+
+with open ("drivers-info.txt", "r") as file:
     data = file.read()
     print(data)
